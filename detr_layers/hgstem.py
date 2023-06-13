550030cadc6300261980afc8fe3a36eb15d9a679
Pranav Durai <pranavdurai10@gmail.com>
2023-06-13 22:50:15 +0530

    Added comments for HGStem.

--- detr_layers/hgstem.py
+++ detr_layers/hgstem.py
@@ -1,7 +1,17 @@
 class HGStem(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(HGStem, self).__init__()
-        # Initialize the layers for HGStem
+        '''
+        HGSTEM implementation.
+        
+        params:
+                in_channels (int): Number of input channels. 
+                out_channels (int): Number of output channels.
+                kernel_size (int or tuple): Size of the convolving kernel.
+                stride (int or tuple): Stride of the convolution.
+                padding (int or tuple): Padding added to all the sides of the input.
+                inplace (bool): If set to True, the input will be modified in-placed. Default: False.
+        '''
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)

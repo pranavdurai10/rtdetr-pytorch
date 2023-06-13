dc18a85d5cdce12b511b70e922d6969120670e1a
Pranav Durai <pranavdurai10@gmail.com>
2023-06-13 22:50:55 +0530

    Added comments for AIFI.

--- detr_layers/aifi.py
+++ detr_layers/aifi.py
@@ -1,7 +1,15 @@
 class AIFI(nn.Module):
     def __init__(self, in_channels, num_repeats):
         super(AIFI, self).__init__()
-        # Initialize the layers for AIFI
+        '''
+        AIFI implementation.
+
+        params:
+                in_channels (int): Number of input channels for AIFI.
+                num_repeats (int): Number of times the block should be repeated.
+        variables:         
+                ModuleList (list): Holds sub-modules in a list. Used to store the layers. Used to store the layer of AIFI.
+        '''
         self.layers = nn.ModuleList()
         for _ in range(num_repeats):
             self.layers.append(nn.Linear(in_channels, in_channels))

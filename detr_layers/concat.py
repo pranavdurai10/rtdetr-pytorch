160360247091804baec36f776e00cde1476c8909
Pranav Durai <pranavdurai10@gmail.com>
2023-06-13 22:51:04 +0530

    Added comments for Concat.

--- detr_layers/concat.py
+++ detr_layers/concat.py
@@ -1,6 +1,13 @@
 class Concat(nn.Module):
     def __init__(self, indices, dim):
         super(Concat, self).__init__()
+        '''
+        Conv implementation. 
+
+        params:
+                indices (list or tuple): Indices of the tensors to concatenate.
+                dim (int): Dimension along which the tensors will be concatenated. 
+        '''
         # Initialize the layers for Concat
         self.indices = indices
         self.dim = dim

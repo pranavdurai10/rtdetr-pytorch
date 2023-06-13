95731f01bd6c8d4f7cb71a8b230f4444494ef392
Pranav Durai <pranavdurai10@gmail.com>
2023-06-13 22:51:37 +0530

    Added comments for RTDETRDecoder.

--- detr_layers/decoder.py
+++ detr_layers/decoder.py
@@ -1,6 +1,12 @@
 class RTDETRDecoder(nn.Module):
     def __init__(self, num_classes):
         super(RTDETRDecoder, self).__init__()
+        '''
+        RTDETRDecoder implementation.
+
+        params:
+                num_classes (int): Number of classes in the dataset.
+        '''
         # Initialize the layers for RTDETRDecoder
         self.num_classes = num_classes
 

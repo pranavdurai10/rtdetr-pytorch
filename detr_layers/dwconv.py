c46c4674ade82659955cc70a11cd16dc7277e420
Pranav Durai <pranavdurai10@gmail.com>
2023-06-13 22:50:44 +0530

    Added comments for DWConv.

--- detr_layers/dwconv.py
+++ detr_layers/dwconv.py
@@ -1,6 +1,17 @@
 class DWConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias):
         super(DWConv, self).__init__()
+        '''
+        DWConv implementation.
+
+        params:
+                in_channels (int): Number of input channels for DWConv.
+                out_channels (int): Number of output channels for DWConv.
+                kernel_size (int or tuple): Size of the convolving kernel.
+                stride (int or tuple): Stride of the kernel.
+                padding (int or tuple): Addition of padding around the kernel.
+                bias (bool): If True, adds a learnable bias to the output.
+        '''
         # Initialize the layers for DWConv
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=in_channels, bias=bias)
         self.bn = nn.BatchNorm2d(out_channels)

2c9c0a2b93931b72a12eb447abbb7ee4e15fa123
Pranav Durai <pranavdurai10@gmail.com>
2023-06-14 20:52:49 +0530

    Added comments for RepC3.

--- detr_layers/repc3.py
+++ detr_layers/repc3.py
@@ -1,6 +1,16 @@
 class RepC3(nn.Module):
     def __init__(self, in_channels):
         super(RepC3, self).__init__()
+        '''
+        RepC3 implementation.
+
+        params: 
+                in_channels (int): Number of input channels.
+                kernel_size (int or tuple): Size of the convolving kernel.
+                stride (int or tuple): Stride of the kernel.
+                padding (int): Addition of padding around the kernel.
+                bias (boolean): If True, adds a learnable bias to the output.
+        '''
         # Initialize the layers for RepC3
         self.layers = nn.Sequential(
             Conv(in_channels, in_channels, kernel_size=3, stride=1, padding=1, bias=False),
